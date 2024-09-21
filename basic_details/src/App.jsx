73de27db6432742af7b362b155cac1daa2111e51
Aniket Ghosh <ghoshaniket00000@gamil.com>
2024-09-21 17:30:32 +0530

    updated basic-details in react

--- basic_details/src/App.jsx
+++ basic_details/src/App.jsx
@@ -1,14 +1,14 @@
 // App.jsx
 import React from "react";
-import Navbar from "./components/Navbar";
+
 import FormPage from "./components/FormPage";
 
 
 const App = () => {
   return (
     <>
-      <Navbar></Navbar>
-      <h1 className="text-3xl font-bold text-gray-900 ml-40 mr-10 mt-7">
+      
+      <h1 className="text-4xl font-bold text-gray-900 ml-20 mr-10 mt-7 mb-18 text-center">
         Basic Details
       </h1>
       <FormPage></FormPage>
